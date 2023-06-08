e6ffdb06deaab062bc039cf6a07214c950e8f664
hj <hae7077.naver.com>
2023-06-08 17:13:17 +0900

    feat: bottom navigation view, fragment 추가

---
+++ app/src/main/java/com/example/team11_project_front/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.team11_project_front;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fm = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+    private MyPageFragment myPageFragment = new MyPageFragment();
+    private QnaFragment qnaFragment = new QnaFragment();
+    private HomeFragment homeFragment = new HomeFragment();
+    private PostFragment postFragment = new PostFragment();
+    private FragmentTransaction ft;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ft = fm.beginTransaction();
+
+        bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        bottomNavigationView.setOnItemSelectedListener(new ItemSelectedListener());
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.menu_frame_layout, homeFragment).commit();
+    }
+
+    class ItemSelectedListener implements NavigationBarView.OnItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            ft = fm.beginTransaction();
+            int itemId = item.getItemId();
+            if (itemId == R.id.menu_home) {
+                ft.replace(R.id.menu_frame_layout, homeFragment);
+                ft.commit();
+                return true;
+            }
+            else if (itemId == R.id.menu_mypage) {
+                ft.replace(R.id.menu_frame_layout, myPageFragment);
+                ft.commit();
+                return true;
+            }
+            else if (itemId == R.id.menu_post) {
+                ft.replace(R.id.menu_frame_layout, postFragment);
+                ft.commit();
+                return true;
+            }
+            else if (itemId == R.id.menu_qna) {
+                ft.replace(R.id.menu_frame_layout, qnaFragment);
+                ft.commit();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/team11_project_front/MyPageFragment.java
@@ -0,0 +1,21 @@
+package com.example.team11_project_front;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MyPageFragment extends Fragment {
+    private View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_my_page, container, false);
+        return view;
+    }
+}
