469decdf4f18c9951f130b7980b90645bef593dd
kimlohen <qmdlrhdfyd@naver.com>
2023-06-22 15:58:26 +0900

    chore: change serverIP

--- app/src/main/java/com/example/team11_project_front/RetrofitClient.java
+++ app/src/main/java/com/example/team11_project_front/RetrofitClient.java
@@ -46,7 +46,7 @@ public class RetrofitClient {
     private static com.example.team11_project_front.API.pictureApi pictureApi;
     private static com.example.team11_project_front.API.ansApi ansApi;
     //사용하고 있는 서버 BASE 주소
-    private static String baseUrl = "http://43.202.5.122/";
+    private static String baseUrl = "http://3.38.104.166/";
 
 
     private RetrofitClient() {
